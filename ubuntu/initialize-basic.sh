4affa1bb65df72e808e1a0436c9760ca4eda0c8f
Anthony Egwu <tony5egwu@gmail.com>
2021-03-08 20:33:39 +0100

    concat added to stages

--- ubuntu/initialize-basic.sh
+++ ubuntu/initialize-basic.sh
@@ -12,20 +12,20 @@ echo "Docker install complete !" &&
 
 
 #docker compose 
-echo "Installing Docker compose ..."&&
+echo "Installing Docker compose ..." &&
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
 sudo chmod +x /usr/local/bin/docker-compose &&
 echo "Docker compose install complete !" &&
 
 #nginx 
-echo "Installing nginx ..."
+echo "Installing nginx ..." &&
 sudo apt update &&
 sudo apt install nginx &&
 echo "Nginx install complete !" &&
 
 
 #Certbot
-echo "Installing certbot ..."
+echo "Installing certbot ..." &&
 sudo apt install certbot python3-certbot-nginx &&
 echo "Certbot install complele ..."
 
