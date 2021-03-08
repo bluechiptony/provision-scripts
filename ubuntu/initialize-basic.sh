38e8a804010e5f2b8522b78b099da6042396533a
Anthony Egwu <tony5egwu@gmail.com>
2021-03-08 20:27:43 +0100

    initial commit

---
+++ ubuntu/initialize-basic.sh
@@ -0,0 +1,28 @@
+#docker 
+echo "Installing Docker ..." &&
+sudo apt update &&
+sudo apt install apt-transport-https ca-certificates curl software-properties-common &&
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" &&
+sudo apt update &&
+#apt-cache policy docker-ce &&
+sudo apt install docker-ce &&
+sudo usermod -aG docker ${USER} &&
+echo "Docker install complete !" &&
+
+
+#docker compose 
+echo "Installing Docker compose ..."&&
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+sudo chmod +x /usr/local/bin/docker-compose &&
+echo "Docker compose install complete !" &&
+
+#nginx 
+echo "Installing nginx ..."
+sudo apt update &&
+sudo apt install nginx &&
+echo "Nginx install complete !" &&
+
+
+
+
